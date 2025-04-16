837d297cd7144609141c7328879d48c345c351b5
urbanec <urbanec@spsejecna.cz>
2025-04-16 11:46:20 +0200

    vytvoreni zakladu pro figurine

--- src/Figurine.java
+++ src/Figurine.java
@@ -1,8 +1,24 @@
 public class Figurine {
 
-    boolean taken;
     boolean white;
-    boolean queen;
-    int[][] position;
+    boolean queen = false;
 
+    int[] position;
+
+    public Figurine(boolean white, int line, int column){
+        this.white = white;
+        this.position = new int[]{line, column};
+    }
+
+
+    public boolean isWhite() {
+        return white;
+    }
+
+    public boolean isQueen() {
+        return queen;
+    }
+    public int[] getPosition() {
+        return position;
+    }
 }
---
+++ src/Game.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Game {
+
+    ArrayList<Figurine> figurines = new ArrayList<>();
+//    private Array[][] gameBoard = new Array[8][8];
+
+    public Game(){
+
+    }
+
+}
