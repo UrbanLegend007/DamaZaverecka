f1cc31d1983f94880f886f97a17d41c357e088da
urbanec <urbanec@spsejecna.cz>
2025-04-16 11:59:15 +0200

    pridani figurek a inicializace

--- src/Game.java
+++ src/Game.java
@@ -1,13 +1,36 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Game {
 
-    ArrayList<Figurine> figurines = new ArrayList<>();
-//    private Array[][] gameBoard = new Array[8][8];
+    ArrayList<Figurine> figurines;
 
     public Game(){
+        figurines = new ArrayList<>();
+    }
 
+    public String inicializationOfFigurines(){
+        for (int i = 0; i < 16; i++) {
+            figurines.add(new Figurine())
+        }
+    }
+
+    public String addFigurine(boolean white, int line, int column){
+        figurines.add()
+    }
+
+    public String loadDefaultPosition(){
+        String line;
+        try (BufferedReader reader = new BufferedReader(new FileReader("res/defaultPosition"))){
+            while((line = reader.readLine()) != null){
+                String parts[] = line.split(";");
+
+            }
+        } catch (Exception e){
+
+        }
     }
 
 }
