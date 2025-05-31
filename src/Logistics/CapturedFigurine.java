e50c3f1f2731a349bba7344a3f4f1e2e9ee614c3
urbanec <urbanec@spsejecna.cz>
2025-05-31 09:57:28 +0200

    pridani capturedFigurine na ukladani figurek, ktery jsem sebral, aby mi potom spravne fungoval undo

---
+++ src/Logistics/CapturedFigurine.java
@@ -0,0 +1,14 @@
+package Logistics;
+
+public class CapturedFigurine {
+    public Figurine figurine;
+    public int row;
+    public int col;
+
+    public CapturedFigurine(Figurine figurine, int row, int col) {
+        this.figurine = figurine;
+        this.row = row;
+        this.col = col;
+    }
+}
+
